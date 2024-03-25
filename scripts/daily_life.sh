47a4c2c5794db77d4243abc893aed7fc26798444
Lutao Jiang <114853034+lutao2021@users.noreply.github.com>
2024-03-25 15:53:07 +0800

    Update daily_life.sh

--- scripts/daily_life.sh
+++ scripts/daily_life.sh
@@ -2,5 +2,5 @@
 python main.py --workspace workspace_daily \
 --iters 600000 --lr 5e-5 --guidance IF --h 512 --w 512 \
 --batch_size 8 --c_batch_size 4 --radius_range 2.5 2.5 --default_radius 2.5 --eval_interval 1 --save_interval 5 \
---port 12369 --fp16 --grad_clip 5.0 --xyzres --perpneg --test_interval 20 --ema_decay 0.99  --side_decay_factor 5.0 --negative_w -3.0 \
+--port 12369 --fp16 --grad_clip 5.0 --xyzres --perpneg --side_decay_factor 5.0 --negative_w -3.0 --test_interval 20 --ema_decay 0.99 \
 --prompts_set daily_life --cache_path ./daily_life_if.pkl
